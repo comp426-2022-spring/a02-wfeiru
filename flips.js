160765e58106910d21d296373594e2ec15b5d180
Ruth Wu <rwu02@email.unc.edu>
2022-02-09 00:37:23 -0500

    flips no argument

--- flips.js
+++ flips.js
@@ -1,11 +1,13 @@
 import { createRequire } from 'module';
 import {coinFlips, countFlips} from './modules/coin.mjs';
 
-let num = 1;
-
 const require = createRequire(import.meta.url)
-const args = require('minimist')(process.argv.slice(2)); //help
-num = args['number'];
+const args = require('minimist')(process.argv.slice(2));
+
+let num = 1;
+if ('number' in args) {
+    num = args['number'];
+}
 
 const flips = coinFlips(num);
 const count = countFlips(flips);
