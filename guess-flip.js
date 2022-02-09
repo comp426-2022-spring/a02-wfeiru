6ece00c9b7c94fee0807441aae71ab32dbd85b08
Ruth Wu <rwu02@email.unc.edu>
2022-02-09 01:02:44 -0500

    guess-flip errors for inputs

--- guess-flip.js
+++ guess-flip.js
@@ -4,6 +4,19 @@ import { flipACoin } from "./modules/coin.mjs";
 const require = createRequire(import.meta.url);
 const args = require('minimist')(process.argv.slice(2));
 
+try {
+    if (!('call' in args)) {
+        throw "no input";
+    }
+    if (args['call'] != 'heads' && args['call'] != 'tails') {
+        throw "invalid input"
+    }
+} catch (err) {
+    console.log('Error: ' + err);
+    console.log('Usage: node guess-flip --call=[heads|tails]');
+    process.exit(1);
+}
+
 let call = args['call'];
 
 const record = flipACoin(call);
